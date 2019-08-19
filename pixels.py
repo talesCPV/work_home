93885527c53c45b032762454ada97988fe6a2626
talesCPV <tales@flexibus.com.br>
2019-08-19 19:48:13 -0300

    1

--- pixels.py
+++ pixels.py
@@ -4,17 +4,26 @@ from tkinter import *
 
 palette = ['black','red','purple','green']
 
+pos = [0,0]
 
 class CHR:
     def __init__(self,raiz):
         self.canvas = Canvas(raiz, width=320, height=320, bg='white')
         self.canvas.pack() 
 
-        altura = 200 # Altura do canvas
-
-        pol=self.canvas.create_polygon
         ret=self.canvas.create_rectangle
-        ret(0, 0, 10, 10, fill=palette[3])
+        cor = 0
+
+        for x in range(32):
+            for y in range(32):
+                ret(pos[0], pos[1], pos[0]+10, pos[1]+10, fill=palette[cor])
+                pos[0] += 10
+                cor += 1
+                if cor == 4:
+                    cor = 0
+
+            pos[0] = 0
+            pos[1] += 10
 
 
 instancia=Tk()
